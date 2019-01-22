f32497d038f2454b8c86980325111cd627a4a00a
jizzhub <niki90h@gmx.de>
2019-01-22 19:17:58 +0100

    outfittery

---
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author niki9
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Schreib mal noch nen paar sachen hier rein und push das dann wieder");
+        System.out.println("Chrisi schreib mal hier was anderes rein");
+    }
+    
+}
