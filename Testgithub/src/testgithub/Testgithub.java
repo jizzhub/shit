cec3ad82046552059ec9e77bac4ef53bf8588589
jizzhub <niki90h@gmx.de>
2019-01-22 13:02:48 +0100

    füge test project hinzu

---
+++ Testgithub/src/testgithub/Testgithub.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+/**
+ *
+ * @author niki9
+ */
+public class Testgithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hunde");
+        Testgithub foo = new Testgithub();
+        foo.sayName();
+    }
+
+    public class Katze {
+
+    }
+
+    public void sayName() {
+        System.out.println("Katttze");
+    }
+}
